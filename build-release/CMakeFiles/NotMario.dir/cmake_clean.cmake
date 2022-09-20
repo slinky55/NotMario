a7745e10d6542e4f5525cad94d79d2feebf3e21c
slinky55 <larry.mason@ufl.edu>
2022-09-20 19:21:28 -0400

    initial commit

---
+++ build-release/CMakeFiles/NotMario.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NotMario.dir/main.cpp.o"
+  "CMakeFiles/NotMario.dir/main.cpp.o.d"
+  "NotMario"
+  "NotMario.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NotMario.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
